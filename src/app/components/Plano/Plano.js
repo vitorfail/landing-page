84e08276bd06c38e3298de89f26e8ed15d88829d
vitorfail <vitor_andrademanoel@hotmail.com>
2023-07-03 15:46:35 -0300

    incrementando o elemento 'Plano'

--- src/app/components/Plano/Plano.js
+++ src/app/components/Plano/Plano.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import Thumb from '../../images/comparativo_img_CTA.png'
-import Butt from '../../images/comparativo_img_CTA.png'
+import Butt from '../../images/selo_RD.png'
+import Cartao from '../../images/no-card-dark.webp'
+import Estrelas from '../../images/rating.webp'
 import './Plano.css'
 export default function Plano(){
     return(
@@ -10,8 +12,18 @@ export default function Plano(){
                 <label>Pronto para triplicar a sua <strong>Geração de Leads?</strong></label>
                 <label>Criação e ativação em <strong>3 minutos</strong></label>
                 <div className='botoes'>
-                    <h3>VER DEMONSTRAÇÃO</h3>
+                    <button>VER DEMONSTRAÇÃO</button>
                     <Image src={Butt}></Image>
+                </div>
+                <div className='avaliacao'>
+                    <div className='frases'>
+                        <Image src={Cartao}></Image>
+                        <h3>Não é necessário Cartão de Crédito</h3>
+                    </div>
+                    <div className='frases'>
+                        <Image src={Estrelas}></Image>
+                        <h3>4.9/5 média de satisfação</h3>
+                    </div>
                 </div>
             </div>
 
